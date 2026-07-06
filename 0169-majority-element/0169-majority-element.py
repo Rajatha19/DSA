94a798040294f25e8c83ef8fc12769f6c30d0ff5
Rajatha <149949532+Rajatha19@users.noreply.github.com>
2026-07-06 17:34:54 +0530

    Time: 3 ms (85.23%), Space: 21.1 MB (86.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,5 @@
-from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count=Counter(nums)
-        return max(count,key=count.get)
+        nums.sort()
+        n=len(nums)
+        return nums[n//2]
