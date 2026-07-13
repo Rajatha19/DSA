42cb76bb4dd61c426f64f289e4bfc35cb043c41b
Rajatha <149949532+Rajatha19@users.noreply.github.com>
2026-07-13 18:32:44 +0530

    Time: 35 ms (26.04%), Space: 25.8 MB (91.93%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n=len(gas)
+        total_surplus,surplus,start=0,0,0
+        for i in range(n):
+            total_surplus+=gas[i]-cost[i]
+            surplus+=gas[i]-cost[i]
+            if surplus<0:
+                surplus=0
+                start=i+1
+        return -1 if (total_surplus<0) else start
